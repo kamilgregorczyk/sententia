7de293214db27d9e37476e8b202fb4414507420e
Kamil Gregorczyk <gregorczyk@me.com>
2016-08-07 14:57:00 +0200

    add local.py

---
+++ sententia/local.py
@@ -0,0 +1,21 @@
+import psycopg2
+from base import *
+SECRET_KEY = '6g@a7pu2g)+cg5@d8zh-mznfnk!r^_&n+%8l+cv+b+@ywz7&c!'
+
+DEBUG = True
+BASE_URL = 'http://localhost:8000'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'postgres',
+        'NAME': 'sententia2',
+        'PASS': 'online13',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'OPTIONS': {
+            'isolation_level': psycopg2.extensions.ISOLATION_LEVEL_SERIALIZABLE,
+        },
+
+    }
+}
