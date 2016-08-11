0300e59cbb0cfc601c9fc176d03abcf1f837299e
Kamil Gregorczyk <gregorczyk@me.com>
2016-08-11 11:04:22 +0200

    test

--- fabfile.py
+++ fabfile.py
@@ -7,7 +7,6 @@ env.hosts = ['s8.mydevil.net']
 PROJECT_PATH = '/usr/home/uniqe15/domains/sententia.uniqe15.usermd.net/public_python'
 venv_path = '/usr/home/uniqe15/.virtualenvs/sententia/bin/python'
 
-
 def deploy():
     run('export DJANGO_SETTINGS_MODULE=sententia.production')
     run('cd %s; git pull' % PROJECT_PATH)
