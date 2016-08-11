b7a2190d773446a110efe8743de6e68e79d567c4
Kamil Gregorczyk <gregorczyk@me.com>
2016-08-11 11:05:17 +0200

    test

--- fabfile.py
+++ fabfile.py
@@ -8,7 +8,6 @@ PROJECT_PATH = '/usr/home/uniqe15/domains/sententia.uniqe15.usermd.net/public_py
 venv_path = '/usr/home/uniqe15/.virtualenvs/sententia/bin/python'
 
 def deploy():
-    run('export DJANGO_SETTINGS_MODULE=sententia.production')
     run('cd %s; git pull' % PROJECT_PATH)
     run('%s %s/manage.py migrate' % (venv_path, PROJECT_PATH))
     run('%s %s/manage.py collectstatic --noinput' % (venv_path, PROJECT_PATH))
