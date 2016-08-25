f431763df2c9afce7765b7ad3c47a969159fab0b
Kamil Gregorczyk <gregorczyk@me.com>
2016-08-25 15:03:10 +0200

    improved poll clone

--- poll/models.py
+++ poll/models.py
@@ -32,7 +32,7 @@ def get_code(cls, filter_params={}):
     i = 0
     while cls.objects.filter(**filter_params).filter(code=code).exists():
         i += 1
-        code = gen_code() if i < 37 else gen_code(3 + (i / 37))
+        code = gen_code() if i < 1000 else gen_code(3 + (i / 1000))
     return code
 
 
